5a5029e53fa7fdb85f9ed9dadc5847653be5f413
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-06 23:21:19 +0200

    Delete backend directory

--- backend/src/routes/license.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/license.controller');
-const authMiddleware = require('../middleware/auth.middleware');
-
-router.post('/generate', authMiddleware.adminOnly, controller.generate);
-
-module.exports = router;
--- backend/src/routes/pair.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/pair.controller');
-const authMiddleware = require('../middleware/auth.middleware');
-
-router.post('/create', authMiddleware.adminOnly, controller.createPairCode);
-router.post('/confirm', controller.confirmPair);
-
-module.exports = router;
