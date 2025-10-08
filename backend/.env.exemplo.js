ad54ede282761ceea23daea8c30a1a7b2dccb9a5
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-08 22:03:20 +0200

    Update .env.exemplo.jsk.

--- backend/.env.exemplo.js
+++ backend/.env.exemplo.js
@@ -2,4 +2,4 @@ PORT=3000
 MONGO_URI=mongodb://localhost:27017/devicemgr
 JWT_SECRET=change_this_secret
 SEED_ADMIN_EMAIL=admin@example.com
-SEED_ADMIN_PASS=admin123
+SEED_ADMIN_PASS=admin123   ADMIN_REGISTRATION_SECRET=put_a_strong_secret_here
