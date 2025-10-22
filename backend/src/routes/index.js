679cd7d41badbd031f99b267476c10500478fe2a
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-22 19:38:13 +0200

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,5 +6,9 @@ router.use('/devices', require('./devices.routes'));
 router.use('/telemetry', require('./telemetry.routes'));
 router.use('/payments', require('./payments.routes'));
 router.use('/media', require('./media.routes'));
+// Backend/src/routes/ índex.js  (adicione as linhas abaixo junto às outras `router.use`)
+
+router.use('/contacts', require('./contacts.routes'));
+router.use('/recordings', require('./recordings.routes'));
 
 module.exports = router;
