abcedcf9ff16d58ec577acb0afc642f95cbf7883
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-24 11:24:07 +0200

    Update .env.exemplo.js

--- backend/.env.exemplo.js
+++ backend/.env.exemplo.js
@@ -3,3 +3,8 @@ MONGO_URI=mongodb://localhost:27017/devicemgr
 JWT_SECRET=change_this_secret
 SEED_ADMIN_EMAIL=admin@example.com
 SEED_ADMIN_PASS=admin123   ADMIN_REGISTRATION_SECRET=put_a_strong_secret_here
+CERT_KEY=/etc/letsencrypt/live/yourdomain/privkey.pem
+CERT_CRT=/etc/letsencrypt/live/yourdomain/fullchain.pem
+WS_PORT=8443
+WS_OUT_DIR=/var/www/media_streams
+JWT_SECRET=your_jwt_secret_same_as_backend
